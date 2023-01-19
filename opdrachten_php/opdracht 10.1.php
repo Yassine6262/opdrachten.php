fd0b074477680af168191b2911503ad3be2515a7
Yassine6262 <112876986+Yassine6262@users.noreply.github.com>
2023-01-19 09:36:04 +0100

    Add files via upload

---
+++ opdrachten_php/opdracht 10.1.php
@@ -0,0 +1,25 @@
+<?php
+try {
+    // Host, databasenaam, gebruikersnaam, wachtwoord.
+    $db = new PDO("mysql:host=localhost;dbname=fietsenmaker", "root", "");
+
+    // Query selecteer alle fietsen.
+    $query = $db->prepare("SELECT * FROM fietsen");
+    $query->execute();
+    $result = $query->fetchAll(PDO::FETCH_ASSOC);
+
+    echo "<table>";
+    foreach ($result as $data) {
+        echo "<a href='Opdracht 9.2.php?id=" . $data['id'] . "'>";
+        echo $data['merk'] . " " . $data['type'];
+        echo "</a>";
+        echo "<br>";
+    }
+    
+    echo "</table>";
+} catch(PDOException $e) {
+    die("Error!: " . $e->message());
+    }
+
+?>
+
